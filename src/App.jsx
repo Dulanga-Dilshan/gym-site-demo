637819e7eb0976f8d455babf04a230bfb3e27080
Dulanga Dilshan <2020ICTS1@vauitc.com>
2026-03-18 13:46:36 +0900

    init react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="min-h-screen bg-white"></div>
+      <h1>test</h1>
+    </>
+  )
+}
+
+export default App
